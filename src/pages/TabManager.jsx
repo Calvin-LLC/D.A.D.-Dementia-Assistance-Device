c4dfa3f6adcfda7d84d018587858751611ed2282
Calvin-LLC <calvinguzman4@gmail.com>
2022-01-27 15:43:45 -0600

    re-coded serverside & other systems to be fully secure

--- src/pages/TabManager.jsx
+++ src/pages/TabManager.jsx
@@ -19,25 +19,6 @@ import Tab1 from "./Tab1";
 import Tab2 from "./Tab2";
 import Tab3 from "./Tab3";
 
-/* Core CSS required for Ionic components to work properly */
-import "@ionic/react/css/core.css";
-
-/* Basic CSS for apps built with Ionic */
-import "@ionic/react/css/normalize.css";
-import "@ionic/react/css/structure.css";
-import "@ionic/react/css/typography.css";
-
-/* Optional CSS utils that can be commented out */
-import "@ionic/react/css/padding.css";
-import "@ionic/react/css/float-elements.css";
-import "@ionic/react/css/text-alignment.css";
-import "@ionic/react/css/text-transformation.css";
-import "@ionic/react/css/flex-utils.css";
-import "@ionic/react/css/display.css";
-
-/* Theme variables */
-import "./../theme/variables.css";
-
 import { save_screen, get_current_screen } from "./data";
 
 const TabManager = () => {
